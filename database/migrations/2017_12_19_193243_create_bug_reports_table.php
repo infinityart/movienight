d56b58425ca5e0cb43af1df59208e83d56b6c059
infinityart <jonty_wolf@hotmail.com>
2017-12-19 22:32:44 +0100

    Added bug reports migration

---
+++ database/migrations/2017_12_19_193243_create_bug_reports_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBugReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bug_reports', function (Blueprint $table) {
+            $table->increments('id', 10);
+            $table->integer('user_id', 10);
+            $table->string('title', 255);
+            $table->text('description');
+            $table->string('img_url', 255);
+            $table->integer('votes', 10);
+            $table->tinyInteger('status', 1);
+            $table->timestamp('created_at');
+
+            $table->index(['title', 'created_at']);
+
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bug_reports');
+    }
+}
