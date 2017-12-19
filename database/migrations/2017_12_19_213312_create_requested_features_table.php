98bae6bc68d060d705e5c5ec2d2607b183a73ca3
infinityart <jonty_wolf@hotmail.com>
2017-12-19 23:04:54 +0100

    Updated support site migrations

--- database/migrations/2017_12_19_213312_create_requested_features_table.php
+++ database/migrations/2017_12_19_213312_create_requested_features_table.php
@@ -15,15 +15,17 @@ class CreateRequestedFeaturesTable extends Migration
     {
         Schema::create('requested_features', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id', 10);
+            $table->integer('user_id');
             $table->string('title', 255);
             $table->text('description');
             $table->string('img_url', 255)->nullable();
-            $table->integer('votes', 10);
-            $table->tinyInteger('status', 1);
+            $table->integer('votes');
+            $table->tinyInteger('status');
             $table->timestamp('created_at');
 
-            $table->index(['title', 'created_at', 'votes']);
+            $table->index('title');
+            $table->index('created_at');
+            $table->index('votes');
 
             $table->foreign('user_id')
                   ->references('id')->on('users')
